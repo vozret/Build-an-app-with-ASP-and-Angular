feee758e3f429cdc3b21555bd8443d9d4dc0139d
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-05-18 15:32:38 +0200

    Initial Commit

---
+++ BabyApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using BabyApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BabyApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
