11c342d828b0d454a16dd71e157a3846f515e712
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-05-19 18:26:46 +0200

    Security 2

--- BabyApp.API/Data/DataContext.cs
+++ BabyApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace BabyApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
