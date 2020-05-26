d26fb4bf71a9ae19a937a27071b4b339daaee7fd
Veronika Ozretic <veronika@Veronikas-MacBook-Pro.local>
2020-05-26 16:45:51 +0200

    adding routing in angular

--- BabyApp-SPA/src/app/nav/nav.component.ts
+++ BabyApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ export class NavComponent implements OnInit {
   model: any = {
   };
 
-  constructor(public authService: AuthService, private aleritfy: AlertifyService) { }
+  constructor(public authService: AuthService, private aleritfy: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +22,8 @@ export class NavComponent implements OnInit {
       this.aleritfy.success('Logged in successfully');
     }, error => {
       this.aleritfy.error('Unauthorized');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -31,6 +34,7 @@ export class NavComponent implements OnInit {
   logout(){
     localStorage.removeItem('token');
     this.aleritfy.message('Logged out');
+    this.router.navigate(['/home']);
   }
 
 }
